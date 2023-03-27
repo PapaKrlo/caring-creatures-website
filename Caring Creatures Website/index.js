3e1cf57a42e701510b3a99f21528465b8fcae25a
vcasteline <vcasteline@ufl.edu>
2023-03-27 21:36:48 +0100

    fixes and added section

--- Caring Creatures Website/index.js
+++ Caring Creatures Website/index.js
@@ -85,3 +85,27 @@ async function mintNFT() {
   document.getElementById("mintButton").addEventListener("click", mintNFT);
 
 }
+// Get the button:
+let mybutton = document.getElementById("btt-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function changeImage(id) {
+  document.getElementById(id).classList.toggle("tag");
+  document.getElementById(id).classList.toggle("tag-open");
+}
