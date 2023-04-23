8899d9f23ecd56892fd41b146f9866e879a00f6e
vcasteline <vcasteline@ufl.edu>
2023-04-23 16:08:21 -0400

    remaining files

--- Caring Creatures Website/index.js
+++ Caring Creatures Website/index.js
@@ -1,30 +1,30 @@
-function updateTimer() {
-  const countdownDate = new Date("2023-04-12T00:00:00").getTime();
-  const now = new Date().getTime();
-  const distance = countdownDate - now;
+// function updateTimer() {
+//   const countdownDate = new Date("2023-04-16T11:00:00").getTime();
+//   const now = new Date().getTime();
+//   const distance = countdownDate - now;
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   const hours = Math.floor(
+//     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+//   );
+//   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.querySelector(".days").innerHTML = days + " <span>Days</span>";
-  document.querySelector(".hours").innerHTML = hours + " <span>Hours</span>";
-  document.querySelector(".minutes").innerHTML =
-    minutes + " <span>Minutes</span>";
-  document.querySelector(".seconds").innerHTML =
-    seconds + " <span>Seconds</span>";
+//   document.querySelector(".days").innerHTML = days + " <span>Days</span>";
+//   document.querySelector(".hours").innerHTML = hours + " <span>Hours</span>";
+//   document.querySelector(".minutes").innerHTML =
+//     minutes + " <span>Minutes</span>";
+//   document.querySelector(".seconds").innerHTML =
+//     seconds + " <span>Seconds</span>";
 
-  if (distance < 0) {
-    clearInterval(timerInterval);
-    document.querySelector("#timer").innerHTML = "The event has started!";
-  }
-}
+//   if (distance < 0) {
+//     clearInterval(timerInterval);
+//     document.querySelector("#timer").innerHTML = "The event has started!";
+//   }
+// }
 
-updateTimer();
-const timerInterval = setInterval(updateTimer, 1000);
+// updateTimer();
+// const timerInterval = setInterval(updateTimer, 1000);
 
 function updatePriceLabel(value) {
   document.getElementById("priceLabel").innerText = value + " ETH";
